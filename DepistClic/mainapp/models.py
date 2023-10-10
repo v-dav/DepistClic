afdaf7a4c36dc94a72239d63be6998521ddecc63
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-10 10:15:06 +0000

    create question model for database

--- DepistClic/mainapp/models.py
+++ DepistClic/mainapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    title = models.TextField()
+    response_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.instruction
