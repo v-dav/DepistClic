144f183732582bb08a52174e3d88a5bc8d76f59f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-10 09:14:45 +0000

    create views and handler functions for home and questions page

--- DepistClic/mainapp/urls.py
+++ DepistClic/mainapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='depistclic-home'),
+    path('questions/', views.questions, name='depistclic-questions'),
 ]
--- DepistClic/mainapp/views.py
+++ DepistClic/mainapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'mainapp/home.html')
+
+
+def questions(request):
+    return render(request, 'mainapp/questions.html')
