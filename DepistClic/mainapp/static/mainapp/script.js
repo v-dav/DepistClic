d4df8782e573042e7e2c7b0cfab61ae080497793
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-11 19:32:48 +0000

    create js method for getting next question by clicking the pass btn

--- DepistClic/mainapp/static/mainapp/script.js
+++ DepistClic/mainapp/static/mainapp/script.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-	
+$(() => {
+	let nextQuestionId = parseInt($("#pass-btn").data('question-id')) + 1
+	$("#pass-btn").click(() => {
+		$.get('/questions/' + nextQuestionId, (data) => {
+			$("#question_title").html(data.title)
+		})
+		nextQuestionId += 1;
+	})
 });
