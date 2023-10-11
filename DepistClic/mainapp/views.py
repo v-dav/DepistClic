8bcd67ccf5b8db2dccd877dbcff9416d7c46f375
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-11 19:32:03 +0000

    create get question function

--- DepistClic/mainapp/views.py
+++ DepistClic/mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Question
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -17,3 +18,15 @@ def questions(request):
 
 def synthese(request):
     return render(request, 'mainapp/synthese.html')
+
+
+def get_question(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+        data = {
+            'title': question.title,
+            'response_type': question.response_type,
+        }
+        return JsonResponse(data)
+    except Question.DoesNotExist:
+        return JsonResponse({'error': "La question n'existe pas"}, status=404)
