7273c2a4c1a9973c3b8ee33f23249fbb85fefd23
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-11 19:31:50 +0000

    add API route for questions

--- DepistClic/mainapp/urls.py
+++ DepistClic/mainapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='depistclic-home'),
     path('questions/', views.questions, name='depistclic-questions'),
+    path('questions/<int:question_id>/', views.get_question, name='depistclic-get_question'),
     path('synthese/', views.synthese, name="depistclic-synthese")
 ]
