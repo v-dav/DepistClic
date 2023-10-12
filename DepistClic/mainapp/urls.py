26356d47722f902435afba3278eb6e8629ef1c08
hug0-cstrs <hugo.casteras@outlook.fr>
2023-10-13 00:58:08 +0200

    add store_value url

--- DepistClic/mainapp/urls.py
+++ DepistClic/mainapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='depistclic-home'),
     path('questions/', views.get_question, name='depistclic-questions'),
     path('questions/<int:question_id>/', views.get_question, name='depistclic-get_question'),
-    path('synthese/', views.synthese, name="depistclic-synthese")
+    path('synthese/', views.synthese, name="depistclic-synthese"),
+    path('store_value/', views.store_value, name='store_value'),
 ]
