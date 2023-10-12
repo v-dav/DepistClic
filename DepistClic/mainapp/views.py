cde6c32916900c7945c272e5516081b4a5a37315
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-12 10:25:27 +0000

    factorize code

--- DepistClic/mainapp/views.py
+++ DepistClic/mainapp/views.py
@@ -8,16 +8,11 @@ def home(request):
     return render(request, 'mainapp/home.html')
 
 
-def questions(request):
-    first_question = Question.objects.first()
-    context = {
-        'question': first_question
-    }
-    return render(request, 'mainapp/questions.html', context)
-
-
-def get_question(request, question_id):
-    question = Question.objects.get(pk=question_id)
+def get_question(request, question_id=None):
+    if question_id is None:
+        question = Question.objects.first()
+    else:
+        question = Question.objects.get(pk=question_id)
     context = {
         'question': question
     }
