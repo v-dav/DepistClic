ea145a0d0c4f21b6cb1a250f14b0d27f05d1c055
hug0-cstrs <hugo.casteras@outlook.fr>
2023-10-13 00:59:10 +0200

    create class Answer

--- DepistClic/mainapp/models.py
+++ DepistClic/mainapp/models.py
@@ -8,3 +8,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.TextField()
+
+    def __str__(self):
+        return self.answer_text
