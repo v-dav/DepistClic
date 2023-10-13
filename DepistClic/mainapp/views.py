c4c3c29e7e95b7b8110d50f1f9851fed4b7e7a4e
Benjamin <benjamin.alazet@gmail.com>
2023-10-13 11:06:51 +0200

    Create Branch Ben and push my modifications

--- DepistClic/mainapp/views.py
+++ DepistClic/mainapp/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Answer
+from django.http import JsonResponse
 
 
 # Create your views here.
 def home(request):
     return render(request, 'mainapp/home.html')
 
+from .models import Answer
 
-def questions(request):
-    first_question = Question.objects.get(pk=2)
+def store_value(request):
+    if request.method == 'POST':
+        # Get the user answer from the POST request
+        user_answer = request.POST.get('user_answer')
+        
+        # Get the current question
+        current_question = Question.objects.first()
+        
+        # Create a new Answer instance and save it to the database
+        answer = Answer(answer_text=user_answer, question=current_question)
+        answer.save()
+        
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False, 'error': 'Methode de requete non valide'})
+
+def get_question(request, question_id=None):
+    queryset = Answer.objects.all()
+    if question_id is None:
+        question = Question.objects.first()
+    else:
+        question = Question.objects.get(pk=question_id)
     context = {
-        'first_question': first_question
+        'question': question,
+        'answers_list': queryset,
     }
     return render(request, 'mainapp/questions.html', context)
-
 
 def synthese(request):
     return render(request, 'mainapp/synthese.html')
