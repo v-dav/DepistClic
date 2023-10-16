8d1184ac46975c48f4bdbf58b5e6f0cdf444460c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-16 16:01:56 +0000

    modified setting for use environ variables for access database

--- DepistClic/DepistClic/settings.py
+++ DepistClic/DepistClic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'DepistClic.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'Yliaze',
-        'PASSWORD': 'Yliaze',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.environ.get('MYSQL_DB_NAME'),
+        'USER': os.environ.get('MYSQL_DB_USER'),
+        'PASSWORD': os.environ.get('MYSQL_DB_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_DB_HOST'),
+        'PORT': os.environ.get('MYSQL_DB_PORT'),
     }
 }
 
