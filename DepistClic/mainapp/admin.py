7221acaf774886cb57483963d969c3e3342952cb
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-16 11:42:11 +0000

    remove answer model

--- DepistClic/mainapp/admin.py
+++ DepistClic/mainapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Question, Answer
+from .models import Question
 
 # Register your models here.
 admin.site.register(Question)
-admin.site.register(Answer)
