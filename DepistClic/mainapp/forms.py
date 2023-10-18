0bdc3d90852a58b9ec7702a1d33b78363bf68c92
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-18 09:31:24 +0000

    add response choices to the Django Forms

--- DepistClic/mainapp/forms.py
+++ DepistClic/mainapp/forms.py
@@ -2,10 +2,20 @@ from django import forms
 
 
 class AnswerBinary(forms.Form):
-    response = forms.BooleanField(required=True, label="Réponse oui/non")
+    RESPONSE_CHOICES = [
+        (True, 'Oui'),
+        (False, 'Non'),
+    ]
+
+    response = forms.ChoiceField(
+        choices=RESPONSE_CHOICES,
+        widget=forms.RadioSelect,
+        label=''
+    )
+
     question_order = forms.IntegerField(widget=forms.HiddenInput())
 
 
 class AnswerInteger(forms.Form):
-    response = forms.IntegerField(required=True, label="Réponse nombre entier")
+    response = forms.IntegerField(required=True, label='')
     question_order = forms.IntegerField(widget=forms.HiddenInput())
