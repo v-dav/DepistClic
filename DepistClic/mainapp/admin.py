d615885b9d4c14d53912bf39ccf1d6a9da963b2c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-21 13:54:42 +0000

    comment screening test admin to check if needed

--- DepistClic/mainapp/admin.py
+++ DepistClic/mainapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Question, Comment, ScreeningTest
+from django.forms import Textarea
+from django.db import models
+
+
 
 # Register your models here.
 admin.site.register(Question)
@@ -8,6 +12,12 @@ admin.site.register(ScreeningTest)
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('comment_area', 'date')
+
+
+# class ScreeningTestAdmin(admin.ModelAdmin):
+#     formfield_overrides = {
+#         models.JSONField: {'widget': Textarea(attrs={'rows': 8, 'cols': 40})},
+#     }
 
 
 admin.site.register(Comment, CommentAdmin)
