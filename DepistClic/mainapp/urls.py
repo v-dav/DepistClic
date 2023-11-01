ce983c3118a045cd2a637d7c482b9babd9691269
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-01 16:24:02 +0100

    change url in url patterns

--- DepistClic/mainapp/urls.py
+++ DepistClic/mainapp/urls.py
@@ -7,11 +7,10 @@ urlpatterns = [
     path('questions/<int:question_order>/', views.get_question,
          name='depistclic-get_question'),
     path('synthese/', views.synthese, name="depistclic-synthese"),
-    path('contact/', views.contact, name="depistclic-contact"),
     path('comment/', views.comment_page, name="depistclic-comment"),
     path('mentions/', views.mentions_legales, name="depistclic-mentions"),
     path('politique/', views.politique_de_confidentialite, name="depistclic-politique"),
     path('cookies/', views.politique_de_cookies, name="depistclic-cookies"),
-    path('equipe', views.equipe, name="depistclic-equipe"),
+    path('equipe/', views.equipe, name="depistclic-equipe"),
     path('outils/', views.outils, name="depistclic-outils"),
 ]
