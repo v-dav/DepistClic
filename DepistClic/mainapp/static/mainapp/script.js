0d6f65b6ecc92f414a1f1222f870d1b863618b0f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-01 19:35:12 +0100

    fix script js

--- DepistClic/mainapp/static/mainapp/script.js
+++ DepistClic/mainapp/static/mainapp/script.js
@@ -1,80 +1,70 @@
 $(() => {
-    // Stores the current page question order
-    let currentQuestionOrder = parseInt($("#pass-btn").data('question-order'));
+	// Stores the current page question order
+	let currentQuestionOrder = parseInt($("#pass-btn").data('question-order'));
 
-    // Pass the question without storing the value
-    $("#pass-btn").click(() => {
-        let nextQuestionOrder = currentQuestionOrder + 1;
-        let url = '/questions/' + nextQuestionOrder;
-        window.location.href = url;
-    });
+	// Pass the question without storing the value
+	$("#pass-btn").click(() => {
+		let nextQuestionOrder = currentQuestionOrder + 1;
+		let url = '/questions/' + nextQuestionOrder;
+		window.location.href = url;
+	});
 
-    // Go back to the previous question
-    $("#back-btn").click(() => {
-        let prevQuestionOrder = currentQuestionOrder - 1;
-        if (prevQuestionOrder >= 1) {
-            let url = '/questions/' + prevQuestionOrder;
-            window.location.href = url;
-        }
-    });
+	// Go back to the previous question
+	$("#back-btn").click(() => {
+		let prevQuestionOrder = currentQuestionOrder - 1;
+		if (prevQuestionOrder >= 1) {
+			let url = '/questions/' + prevQuestionOrder;
+			window.location.href = url;
+		}
+	});
+	
+	// Go home button with alert
+	$("#home-btn").click(() => {
+		const confirmation = window.confirm("Voulez-vous vraiment revenir, vous allez perdre les données en cours")
+		if (confirmation) {
+			window.location.href = "/";
+		}
+	});
 
-    // Go home button with alert
-    $("#home-btn").click(() => {
-        const confirmation = window.confirm("Voulez-vous vraiment revenir, vous allez perdre les données en cours")
-        if (confirmation) {
-            window.location.href = "/";
-        }
-    });
+	// Submit the form when one input is chosen
+	$('input[type="radio"]').on('change', function() {
+		$(this).closest('form').submit();
+	});
 
-    // Submit the form when one input is chosen
-    $('input[type="radio"]').on('change', function() {
-        $(this).closest('form').submit();
-    });
+	// Selects all elements with the info-icon class
+	$('.info-icon').click(function(){
+		// Get the ID of the modal to display
+		var modalId = $(this).data('modal-toggle');
+		// Selects modal by ID
+		var modal = $('#' + modalId);
+		// Display modal
+		modal.removeClass('hidden');
+		// Hide modal
+		modal.find('.modal-close').click(function(){
+			modal.addClass('hidden');
+			// Keep scroll
+			$('body').css('overflow', 'auto');
+		});
 
-    // Toggle the visibility of the info card
-    $(document).on('click', '.info-icon', function(event) {
-        event.stopPropagation();
-        var testId = $(this).data('test-id');
-        var infoCard = $('.info-card[data-test-id="' + testId + '"]');
-        infoCard.toggleClass('hidden');
-    });
+		// Hide modal when the user click outside the content
+		modal.click(function(event) {
+			if (event.target === this) {
+				//Hide modal
+				modal.addClass('hidden');
+				// Keep scroll
+				$('body').css('overflow', 'auto');
+			}
+		});
+	});
 
-    // Hide the visibility of the info card
-    $(document).click(function(event) {
-        // If the click event occurred outside any info card
-        if (!$(event.target).closest('.info-card').length && !$(event.target).is('.info-icon')) {
-            $('.info-card').addClass('hidden');
-        }
-    });
+	// Add shadow when scrollPosition > 0
+	$(window).scroll(function() {
+		var scrollPosition = $(this).scrollTop();
 
-    // Stores the state of the menu
-    let isMenuOpen = false;
-
-    // Toggle the visibility of the menu on menu button click
-    $("#menu-toggle").click(() => {
-        if (isMenuOpen) {
-            $("#menu").addClass("hidden");
-        } else {
-            $("#menu").removeClass("hidden");
-        }
-        isMenuOpen = !isMenuOpen;
-    });
-
-    // Handle clicks on menu items
-    $("#menu").click(() => {
-        // Close the menu when a menu item is clicked
-        $("#menu").addClass("hidden");
-        isMenuOpen = false;
-    });
-
-    // Add shadow when scrollPosition > 0
-    $(window).scroll(function() {
-        var scrollPosition = $(this).scrollTop();
-
-        if (scrollPosition > 0) {
-            $('header').addClass('shadow-lg transition duration-300');
-        } else {
-            $('header').removeClass('shadow-lg transition duration-300');
-        }
-    });
+		if (scrollPosition > 0) {
+			$('header').addClass('shadow-lg transition duration-300');
+		} else {
+			$('header').removeClass('shadow-lg transition duration-300');
+		}
+	});
 });
