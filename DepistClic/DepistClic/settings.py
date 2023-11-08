76c592ca81166fced89bf3d9f28cfd401e870534
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-08 10:10:20 +0100

    remove a config variable

--- DepistClic/DepistClic/settings.py
+++ DepistClic/DepistClic/settings.py
@@ -43,7 +43,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
