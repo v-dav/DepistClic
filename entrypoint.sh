d86c1a691519ac87da2976cfdf85181cf963c404
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-08 18:26:19 +0100

    remove nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,4 @@
 cd DepistClic
 pipenv run python manage.py migrate --no-input
 pipenv run python manage.py collectstatic --no-input
-ln -s /etc/nginx/sites-available/depistclic /etc/nginx/sites-enabled
-nginx -t
-service nginx start
-service nginx status
 pipenv run gunicorn DepistClic.wsgi:application --bind 0.0.0.0:$PORT
