75911381110874ee90bcf679b887da9e777f1e5a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-08 18:52:55 +0100

    remove useless line

--- DepistClic/DepistClic/urls.py
+++ DepistClic/DepistClic/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 urlpatterns += [ re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
