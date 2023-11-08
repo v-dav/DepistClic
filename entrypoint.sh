3eb2cda513184f6a6b6abec2b052156bf61dc5e2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-08 11:50:35 +0100

    add nginx processes to start

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 cd DepistClic
 pipenv run python manage.py migrate --no-input
 pipenv run python manage.py collectstatic --no-input
+service nginx start
+service nginx status
 pipenv run gunicorn DepistClic.wsgi:application --bind 0.0.0.0:$PORT
