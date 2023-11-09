a95e8da920bd3126ecb2b562192e424f778ed5bd
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-09 11:27:51 +0100

    remplace port by env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd DepistClic
+
 pwd
 ls -la uploads/images
 ls -la uploads/ordonnances
@@ -8,6 +9,8 @@ ls -la uploads/ordonnances
 mkdir static
 pipenv run python manage.py migrate --no-input
 pipenv run python manage.py collectstatic --no-input
+
+envsubst < /etc/nginx/sites-available/depistclic > /etc/nginx/sites-available/depistclic
 
 ln -s /etc/nginx/sites-available/depistclic /etc/nginx/sites-enabled	
 nginx -t	
