5c47f3451c33cf79c9b9d6a2b6693b81828fb62b
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-09 11:02:17 +0100

    nginx config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 cd DepistClic
+pwd
 ls -la uploads/images
 ls -la uploads/ordonnances
+
 mkdir static
 pipenv run python manage.py migrate --no-input
 pipenv run python manage.py collectstatic --no-input
+
+ln -s /etc/nginx/sites-available/depistclic /etc/nginx/sites-enabled	
+nginx -t	
+service nginx start	
+service nginx status
+
 pipenv run gunicorn DepistClic.wsgi:application --bind 0.0.0.0:$PORT --access-logfile -
