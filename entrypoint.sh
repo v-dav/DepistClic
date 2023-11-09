3d1c4514fb50951f38a645df5f2ef58bb36e03cf
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-09 11:14:47 +0100

    inverse order of execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ pipenv run python manage.py collectstatic --no-input
 
 ln -s /etc/nginx/sites-available/depistclic /etc/nginx/sites-enabled	
 nginx -t	
-service nginx start	
-service nginx status
 
 pipenv run gunicorn DepistClic.wsgi:application --bind 0.0.0.0:$PORT --access-logfile -
+service nginx start	
+service nginx status
