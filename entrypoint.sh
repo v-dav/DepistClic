0705004c8659232a87661fefe60f69599248a41e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-09 11:44:56 +0100

    trynig things

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,17 @@ mkdir static
 pipenv run python manage.py migrate --no-input
 pipenv run python manage.py collectstatic --no-input
 
+chmod -R 755 /app/DepistClic/uploads
+
 envsubst < /etc/nginx/sites-available/depistclic > /etc/nginx/sites-available/depistclic
 cat /etc/nginx/sites-available/depistclic
+cat /etc/nginx/nginx.conf
 
-ln -s /etc/nginx/sites-available/depistclic /etc/nginx/sites-enabled	
+ln -s /etc/nginx/sites-available/depistclic /etc/nginx/sites-enabled
+cat /etc/nginx/nginx.conf	
 nginx -t
 service nginx start	
-service nginx status	
+service nginx status
+cat /var/log/nginx/error.log
 
 pipenv run gunicorn DepistClic.wsgi:application --bind 0.0.0.0:$PORT --access-logfile -
