260ffc1462bb173d1003ab642cec0fb8c545ee30
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-11-13 11:59:21 +0100

    add configs to site

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cd DepistClic
 mkdir static
 pipenv run python manage.py migrate --no-input
 pipenv run python manage.py collectstatic --no-input
-pipenv run gunicorn -w 8 DepistClic.wsgi:application --bind 0.0.0.0:$PORT --access-logfile - 
+pipenv run gunicorn DepistClic.wsgi:application --bind 0.0.0.0:$PORT --access-logfile - -w 8 --timeout 120 --log-level debug
